77595facc5105eb3f53e95b9b031c0c96719cc80
Mikhail <m.kaminskiy@list.ru>
2022-09-26 18:22:55 +0700

    task5.py

---
+++ task5.py
@@ -0,0 +1,24 @@
+import math
+
+print('Задача по нахождению расстояния между точками в 2D пространстве')
+print('------------------------------')
+
+
+def getDistance(x1, y1, x2, y2):
+    distanceX = x1 - x2
+    distanceY = y1 - y2
+
+    return math.sqrt(getQuadro(distanceX) + getQuadro(distanceY))
+
+
+def getQuadro(number):
+    return number * number
+
+
+x1Coordinate = int(input('Введите координату X1: '))
+y1Coordinate = int(input('Введите координату Y1: '))
+
+x2Coordinate = int(input('Введите координату X2: '))
+y2Coordinate = int(input('Введите координату Y2: '))
+
+print(getDistance(x1Coordinate, y1Coordinate, x2Coordinate, y2Coordinate))
