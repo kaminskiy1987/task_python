bc322bc4a583d7007e6540c89bcb5119dd7971f7
Mikhail <m.kaminskiy@list.ru>
2022-09-28 22:01:30 +0700

    task8.py

---
+++ task8.py
@@ -0,0 +1,17 @@
+# Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+import random
+
+print("Введите число N: ")
+N = int(input())
+
+array = [random.randint(1, N) for x in range(1, N + 1)]
+
+def calcSerial(n):
+    return (1 + (1 / n)) ** n
+
+
+elementsSum = 0
+for i in array:
+    elementsSum += calcSerial(i)
+
+print("Сумма элементов последовательности : ", elementsSum)
